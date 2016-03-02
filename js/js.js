2152c950401b9d0626cb79ef04f5711fc39abbda
Sami Shelah <sami.shaleh@icloud.com>
2016-03-02 20:27:24 +0300

    added async option

--- js/js.js
+++ js/js.js
@@ -1,7 +1,84 @@
-jQuery(document).ready(function($) {
-    // a selection of Instagram posts
-    // eg. https://instagram.com/p/8gMTgjo6lK/
-    var instalinks = [
+function getInstagramData(a, target, num) {
+    var count = num,
+        countEl = 0;
+    jQuery.each(a, function(i, d) {
+        if (i < count) {
+            jQuery.when(jQuery.ajax({
+                dataType: "json",
+                url: "php/instagram.php?media=" + d,
+                cache: false,
+                async: true,
+                error: function(xhr, status, error) {
+                    count = count + 1;
+                    console.log(status + ' / ' + error + ' / ' + count);
+                }
+            })).then(function(data, textStatus, jqXHR) {
+                console.log(i + ' / ' + textStatus + ' / ' + count);
+                countEl++;
+                var img = '<img src="' + data.entry_data.PostPage[0].media.display_src + '" alt="' + data.entry_data.PostPage[0].media.caption + '" />',
+                    output = '<li data-id="' + countEl + '"><p class="num">' + pad(countEl, 2, 0) + '</p>';
+                output += '<div class="image-box">';
+                output += '<a href="https://instagram.com/p/' + data.entry_data.PostPage[0].media.code + '" target="_blank">' + img + '</a>';
+                output += '</div>';
+                output += '<p class="desc">' + data.entry_data.PostPage[0].media.caption + '</p>';
+                output += '<p class="author"><strong>' + data.entry_data.PostPage[0].media.owner.username + '</strong> </p>';
+                output += '</li>';
+                target.append(output);
+            }).promise().done(function() {
+                sortinstalinks();
+            });
+        } else {
+            return;
+        }
+    });
+}
+
+function sortinstalinks() {
+    jQuery('#instgramelement li').sort(function(a, b) {
+        var an = a.getAttribute('data-id'),
+            bn = b.getAttribute('data-id');
+        if (an > bn) {
+            return 1;
+        }
+        if (an < bn) {
+            return -1;
+        }
+        return 0;
+    }).detach().prependTo(jQuery('#instgramelement'));
+}
+
+// Usage: pad([number], [number], [string]) // pad(1, 2, 0)
+function pad(a, s, d) {
+    d = d || '0';
+    a = a + '';
+    return a.length >= s ? a : new Array(s - a.length + 1).join(d) + a;
+}
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+jQuery(document).on({
+    ready: function() {
+        // a selection of Instagram posts
+        // eg. https://instagram.com/p/8gMTgjo6lK/
+
+        // links without errors
+
+        var instalinks = [
             'BBG2NsYkgVB',
             'BBC3msDm3cc',
             'BBDNddZPKsx',
@@ -17,77 +94,32 @@ jQuery(document).ready(function($) {
             'BBAolZ1iEb8',
             '88qlkdiEfD',
             'BAzlDkePKke'
-        ],
-        instalinksrand = shuffle(instalinks),
-        instgramelement = jQuery('#instgramelement');
+        ];
 
-    function getInstagramData(a) {
-        jQuery.each(a, function(i, d) {
-            jQuery.ajax({
-                cache: false,
-                dataType: "json", // or "jsonp" if we enabled it
-                url: "php/instagram.php?media=" + d,
-                success: function(data) {
-                    var img = '<img src="' + data.entry_data.PostPage[0].media.display_src + '" alt="' + data.entry_data.PostPage[0].media.caption + '" />',
-                    output = '<li data-id="' + i + '"><p class="num">' + pad(i + 1, 2, 0) + '</p>';
-                    output += '<div class="image-box">';
-                    output += '<a href="https://instagram.com/p/' + data.entry_data.PostPage[0].media.code + '" target="_blank">' + img + '</a>';
-                    output += '</div>';
-                    output += '<p class="desc">' + data.entry_data.PostPage[0].media.caption + '</p>';
-                    output += '<p class="author"><strong>' + data.entry_data.PostPage[0].media.owner.username + '</strong> </p>';
-                    output += '</li>';
-                    instgramelement.append(output);
-                },
-                error: function(xhr, status, error) {
-                    console.log('instagram code error: '+ error + '\n' + status);
-                }
-            }).promise().done(function() {
-                sortinstalinks();
-            });
-            // how many items to return, count from 0
-            return i < 4;
-        });
+        /*
+                // links with errors
+                var instalinks = [
+                    'xxxxxx',
+                    'xxxxxx',
+                    'BBDNddZPKsx',
+                    'xxxxxx',
+                    'xxxxxx',
+                    'xxxxxx',
+                    'xxxxxx',
+                    'BBEIIlioTSF',
+                    'BA-vDsLKowA',
+                    'BBGLtUCKo0S',
+                    'BBAcH-LCEUD',
+                    'xxxxxx',
+                    'xxxxxx',
+                    'xxxxxx',
+                    'BAzlDkePKke'
+                ];
+        */
+        // shuffle the links to that each time the page loads it shows different links.
+        var instalinksrand = shuffle(instalinks);
+
+        getInstagramData(instalinksrand, jQuery('#instgramelement'), 5);
     }
 
-    function sortinstalinks() {
-        instgramelement.children('li').sort(function(a, b) {
-                var an = a.getAttribute('data-id'),
-                    bn = b.getAttribute('data-id');
-
-                if (an > bn) {
-                    return 1;
-                }
-                if (an < bn) {
-                    return -1;
-                }
-                return 0;
-            })
-            .detach().prependTo(instgramelement);
-    }
-
-    // Usage: pad([number], [number], [string]) // pad(1, 2, 0)
-    function pad(a, s, d) {
-        d = d || '0';
-        a = a + '';
-        return a.length >= s ? a : new Array(s - a.length + 1).join(d) + a;
-    }
-
-    function shuffle(array) {
-        var currentIndex = array.length,
-            temporaryValue, randomIndex;
-
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
-    }
-
-    getInstagramData(instalinksrand);
 });
