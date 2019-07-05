1227fd4854823e5099813836663b19c927cbfb61
Max Samuelson <maxim.samuelson@icloud.com>
2019-07-05 20:42:53 +0300

    Misc changes.

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
 # getInstagram
-Asynchronously fetches Instagram posts from a provided list of Instagram post IDs via XMLHttpRequest, Javascript and PHP,  skips invalid posts and timed out requests, randomises and renders the resulting data to HTML.
+- Asynchronously fetches Instagram posts from a provided list of Instagram post IDs via XMLHttpRequest
+- Requests to instagram are made from the server side and sent back to the client
+- Skips invalid posts and timed out requests
+- Optinally randomize and renders the resulting data to HTML
+- Templating easily customizable
 
 Example: https://zenitht.com/git/getInstagram/
 
-## License ##
+## License
 
 This package is licensed under MIT license. See LICENSE for details.
+
+See index.php for examples.
+
+## Usage
+
+Option 1:
+Provide a list of post IDs (no posts limit):
+````
+window.instalinks = [
+    'BA9M58hoTYL',
+    'BA-vDsLKowA',
+    'xxxxxx', // invalid post ID, will be skipped
+    '88qlkdiEfD',
+    'BAzlDkePKke'
+];
+
+window.getInstagram(
+  "php/instagram.php",
+  instalinks,
+  document.getElementById('instgramelement'),
+  5,
+  false
+);
+````
+
+Option 2: Provide an instagram user ID (maximum 12 posts can be fetched):
+```
+window.getInstagram(
+  "php/instagram.php",
+  false,
+  document.getElementById('instgramelement'),
+  12,
+  'facebook'
+);
+```
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
     	<meta charset="UTF-8">
     	<title>Instagram via HTTP</title>
-    	<link rel="stylesheet" href="css/css.css">
+    	<link rel="stylesheet" href="css/css.css" type="text/css" />
     </head>
     <body>
     	<ul id="instgramelement">
@@ -14,37 +14,6 @@
     <script rel="text/javascript" src="js/getInstagram.js"></script>
 
     <script type="text/javascript">
-        // Set a selection of Instagram posts by their IDs
-        // includes some bad requests for testing.
-        window.instalinks = [
-            'BA9M58hoTYL',
-            'BA-vDsLKowA',
-            'xxxxxx', // bad request
-            'BAhlW4QvKhB',
-            'BBGLtUCKo0S',
-            'xxxxxx', // bad request
-            'BBG2NsYkgVB',
-            'BBC3msDm3cc',
-            'xxxxxx', // bad request
-            'BBDNddZPKsx',
-            'BAFhPC0mWwY',
-            'BAxb0t1BFf6',
-            'BADJZBgBFRO',
-            'xxxxxx', // bad request
-            'BBEIIlioTSF',
-            'BBAcH-LCEUD',
-            'xxxxxx', // bad request
-            '88qlkdiEfD',
-            'BAzlDkePKke'
-        ];
-
-        // Optional is supplying the list of post IDs, shuffle the links so that they're in random order.
-        // instalinksrand = shuffle(instalinks);
-
-        // Generate from supplied list above
-        // window.getInstagram("php/instagram.php", instalinks, document.getElementById('instgramelement'), 5, false);
-
-        // Latest posts by user ID, maximum 12
         window.getInstagram("php/instagram.php", false, document.getElementById('instgramelement'), 12, 'facebook');
     </script>
 </html>
